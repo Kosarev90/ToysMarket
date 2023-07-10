0fc7db8a2a8401f53d870cf8ce3161d6390ef9af
kosarev90 <kosarev90sv@icloud.com>
2023-07-10 18:47:58 +0300

    добавлен класс Toys.

--- src/ru/kosarev/Toys.java
+++ src/ru/kosarev/Toys.java
@@ -1,2 +1,26 @@
-package ru.kosarev;public class Toys {
+package ru.kosarev;
+
+public class Toys {
+
+    int id;
+    String name;
+    int frequency;
+
+    public Toys(int id, String name, int frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
 }
