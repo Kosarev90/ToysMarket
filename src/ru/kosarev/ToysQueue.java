9007f71d72d24f08ffa532de08ed119d39d220dd
kosarev90 <kosarev90sv@icloud.com>
2023-07-10 19:06:39 +0300

    добавлен класс ToysQues.

---
+++ src/ru/kosarev/ToysQueue.java
@@ -0,0 +1,16 @@
+package ru.kosarev;
+
+public class ToysQueue {
+
+    public ToysQueue(String[] toyArray) {
+
+    }
+
+    public static void main(String[] args) {
+
+        String[] toyArray = {"1 2 конструктор", "2 2 машинка", "3 6 кукла"};
+
+        ToysQueue toysQueue = new ToysQueue(toyArray);
+    }
+
+}
